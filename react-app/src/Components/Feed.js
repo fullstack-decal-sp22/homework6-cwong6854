487f666c749d152cd9cbe2c32a8e017017e5ffa5
Curtis Wong <cwong990@berkeley.edu>
2022-04-08 23:37:18 -0700

    task3

--- react-app/src/Components/Feed.js
+++ react-app/src/Components/Feed.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Post from "./Post";
 import NewPost from "./NewPost";
 
 const Feed = () => {
-  const data = [
-    {
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-      "id": 3,
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-    },
-  ]
+
+  const [data, setData] = useState("");
+
+  const getPostData = () => {
+    // fetch data
+    axios
+    .get("http://localhost:3002/posts")
+    //  wait for promise to be resolved using .then, set state variable data to returned data
+    .then((data) => setData(data.data))
+    .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
 
   return (
     <div style={{ maxWidth: '600px', marginLeft: 'auto', marginRight: 'auto'}}>
@@ -28,12 +27,9 @@ const Feed = () => {
           <Post title={d.title} body={d.body} key={d.id} />
         )
       }
-
+      
       <NewPost/>
     </div>
   )
-
 }
-
-
 export default Feed;
